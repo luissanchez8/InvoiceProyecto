909c5c94a2a4009f0dae02e75b1fa275213ff0d9
luissanchez8 <lsanchez@linkealia.com>
2025-09-26 11:41:48 +0200

    Update EstimatesTab.vue

--- resources/scripts/admin/views/settings/customization/estimates/EstimatesTab.vue
+++ resources/scripts/admin/views/settings/customization/estimates/EstimatesTab.vue
@@ -11,13 +11,13 @@
 
   <BaseDivider class="my-8" />
 
-  <!-- SOLO asistencia ve “Formatos por defecto” -->
+  <!-- “Formatos por defecto” solo asistencia -->
   <EstimatesTabDefaultFormats v-if="isAsistencia" />
   <BaseDivider class="my-8" v-if="isAsistencia" />
 
-  <BaseDivider class="mt-6 mb-2" />
-
-  <ul class="divide-y divide-gray-200">
+  <!-- “Enviar cotización como adjunto” solo asistencia -->
+  <BaseDivider class="mt-6 mb-2" v-if="isAsistencia" />
+  <ul class="divide-y divide-gray-200" v-if="isAsistencia">
     <BaseSwitchSection
       v-model="sendAsAttachmentField"
       :title="$t('settings.customization.estimates.estimate_email_attachment')"
@@ -34,7 +34,6 @@
 import { computed, reactive, inject } from 'vue'
 import { useCompanyStore } from '@/scripts/admin/stores/company'
 import { useUserStore } from '@/scripts/admin/stores/user'
-
 import EstimatesTabEstimateNumber from './EstimatesTabEstimateNumber.vue'
 import EstimatesTabExpiryDate from './EstimatesTabExpiryDate.vue'
 import EstimatesTabDefaultFormats from './EstimatesTabDefaultFormats.vue'
@@ -44,16 +43,20 @@ const utils = inject('utils')
 const companyStore = useCompanyStore()
 const userStore = useUserStore()
 
-// Solo asistencia puede ver Formatos por defecto
 const isAsistencia = computed(() => userStore.currentUser?.role === 'asistencia')
 
 const estimateSettings = reactive({
-  estimate_email_attachment: null,
+  // por defecto ACTIVADO
+  estimate_email_attachment: 'YES',
 })
 
 utils.mergeSettings(estimateSettings, {
   ...companyStore.selectedCompanySettings,
 })
+
+if (!estimateSettings.estimate_email_attachment) {
+  estimateSettings.estimate_email_attachment = 'YES'
+}
 
 const sendAsAttachmentField = computed({
   get: () => estimateSettings.estimate_email_attachment === 'YES',
@@ -61,10 +64,7 @@ const sendAsAttachmentField = computed({
     const value = newValue ? 'YES' : 'NO'
     const data = { settings: { estimate_email_attachment: value } }
     estimateSettings.estimate_email_attachment = value
-    await companyStore.updateCompanySettings({
-      data,
-      message: 'general.setting_updated',
-    })
+    await companyStore.updateCompanySettings({ data, message: 'general.setting_updated' })
   },
 })
 </script>
