f812d3f192d50f903a53964b02f577c9c9fa3801
luissanchez8 <lsanchez@linkealia.com>
2025-09-26 11:43:56 +0200

    Update admin-router.js

--- resources/scripts/admin/admin-router.js
+++ resources/scripts/admin/admin-router.js
@@ -332,7 +332,7 @@ export default [
           {
             path: 'pdf-generation',
             name: 'pdf.generation',
-            meta: { isOwner: false },
+            meta: { isOwner: true },
             component: PDFGenerationSettings,
           },
         ],
