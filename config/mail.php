803b54dab613c6ad47a30d5f2906b23343627057
LINKEALIA <soporte@linkealia.com>
2026-04-13 07:25:13 +0000

    Fix: deshabilitar verificación SSL en SMTP (certificado auto-firmado)

--- config/mail.php
+++ config/mail.php
@@ -1,21 +1,18 @@
 <?php
-
 return [
-
     'driver' => env('MAIL_DRIVER', 'smtp'),
-
     'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-
     'port' => env('MAIL_PORT', 587),
-
     'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-
     'username' => env('MAIL_USERNAME'),
-
     'password' => env('MAIL_PASSWORD'),
-
     'sendmail' => '/usr/sbin/sendmail -bs',
-
     'log_channel' => env('MAIL_LOG_CHANNEL'),
-
+    'stream' => [
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+        ],
+    ],
 ];
