f8ab34c3518cd1fd6c22974a1057a2f1461d01ce
luissanchez8 <lsanchez@linkealia.com>
2026-04-14 10:09:53 +0000

    Fix: reemplazar placeholders en emails de proforma/albarán + textos en español

--- app/Http/Controllers/V1/Admin/DeliveryNote/SendDeliveryNoteController.php
+++ app/Http/Controllers/V1/Admin/DeliveryNote/SendDeliveryNoteController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers\V1\Admin\DeliveryNote;
-
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SendInvoiceRequest;
 use App\Mail\SendInvoiceMail;
@@ -9,30 +7,26 @@ use App\Models\Company;
 use App\Models\CompanySetting;
 use App\Models\DeliveryNote;
 use Illuminate\Support\Facades\Mail;
-
 class SendDeliveryNoteController extends Controller
 {
     public function __invoke(SendInvoiceRequest $request, DeliveryNote $delivery_note)
     {
         $this->authorize('send delivery note', $delivery_note);
-
         $data = $request->all();
         $data['invoice'] = $delivery_note->toArray();
         $data['invoice']['invoice_number'] = $delivery_note->delivery_note_number;
         $data['customer'] = $delivery_note->customer->toArray();
         $data['company'] = Company::find($delivery_note->company_id);
-
+        $data['subject'] = $delivery_note->getEmailString($data['subject'] ?? '');
+        $data['body'] = $delivery_note->getEmailString($data['body'] ?? '');
         $attachPdf = CompanySetting::getSetting('invoice_email_attachment', $delivery_note->company_id) !== 'NO';
         $data['attach']['data'] = $attachPdf ? $delivery_note->getPDFData() : null;
-
         Mail::to($data['to'])->send(new SendInvoiceMail($data));
-
         if ($delivery_note->status == DeliveryNote::STATUS_DRAFT) {
             $delivery_note->status = DeliveryNote::STATUS_SENT;
             $delivery_note->sent = true;
             $delivery_note->save();
         }
-
         return response()->json(['success' => true]);
     }
 }
--- app/Http/Controllers/V1/Admin/ProformaInvoice/SendProformaInvoiceController.php
+++ app/Http/Controllers/V1/Admin/ProformaInvoice/SendProformaInvoiceController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers\V1\Admin\ProformaInvoice;
-
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SendInvoiceRequest;
 use App\Mail\SendInvoiceMail;
@@ -9,30 +7,26 @@ use App\Models\Company;
 use App\Models\CompanySetting;
 use App\Models\ProformaInvoice;
 use Illuminate\Support\Facades\Mail;
-
 class SendProformaInvoiceController extends Controller
 {
     public function __invoke(SendInvoiceRequest $request, ProformaInvoice $proforma_invoice)
     {
         $this->authorize('send proforma invoice', $proforma_invoice);
-
         $data = $request->all();
         $data['invoice'] = $proforma_invoice->toArray();
         $data['invoice']['invoice_number'] = $proforma_invoice->proforma_invoice_number;
         $data['customer'] = $proforma_invoice->customer->toArray();
         $data['company'] = Company::find($proforma_invoice->company_id);
-
+        $data['subject'] = $proforma_invoice->getEmailString($data['subject'] ?? '');
+        $data['body'] = $proforma_invoice->getEmailString($data['body'] ?? '');
         $attachPdf = CompanySetting::getSetting('invoice_email_attachment', $proforma_invoice->company_id) !== 'NO';
         $data['attach']['data'] = $attachPdf ? $proforma_invoice->getPDFData() : null;
-
         Mail::to($data['to'])->send(new SendInvoiceMail($data));
-
         if ($proforma_invoice->status == ProformaInvoice::STATUS_DRAFT) {
             $proforma_invoice->status = ProformaInvoice::STATUS_SENT;
             $proforma_invoice->sent = true;
             $proforma_invoice->save();
         }
-
         return response()->json(['success' => true]);
     }
 }
