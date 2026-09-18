99f7c829130d925012fdf4ef4d9d613ad7c8ef0f
LINKEALIA <soporte@linkealia.com>
2026-09-18 06:15:31 +0000

    fix(rgpd): registrar el middleware de IP real

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,6 +28,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
             \App\Http\Middleware\TrimStrings::class,
             \App\Http\Middleware\TrustProxies::class,
+            \App\Http\Middleware\RealIpBehindCloudflare::class,
             \App\Http\Middleware\ConfigMiddleware::class,
         ]);
 
